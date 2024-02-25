553bff2974e096c6421e07d574e0fccbb9cb3305
OmarNaru <nadaelomr391@gmail.com>
2024-02-25 17:00:25 +0200

    some explanation + uml

--- Strategy Pattern/Program.cs
+++ Strategy Pattern/Program.cs
@@ -17,7 +17,6 @@
             hugeList.Sort();
             hugeList.Print();
         
-        
         }
     }
 }
---
+++ Template Method Pattern/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Template_Method_Pattern
+{
+    internal class Program
+    {
+        /*
+            take a look: https://refactoring.guru/design-patterns/template-method
+            i don't have any example sorry 
+            couldn't find appropriate one 
+            but this pattern is massively used by framework makers I guess
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
